ae655592f03469d46e14f9868c32912369ab0707
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2026-03-28 00:14:17 -0300

    enviando projeto

---
+++ src/test/java/br/com/dev_livery/DevLiveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev_livery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevLiveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
